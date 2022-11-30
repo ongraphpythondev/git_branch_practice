0602b0f07d91d39be166e0438b621e1dd8750290
ongraphpythondev <pythondev@ongraph.com>
2022-12-01 01:18:15 +0530

    commit on branch main

---
+++ practice/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+
+class Test(View):
+    print("this is test1")
+    print("this is test2")
+    print("this is test3")
+    print("this is test4")
+    print("this is test5")
+    print("this is test6")
+    print("this is test7")
+
+
